f4c229dedcaecdf1418ead6646f971c5db7517fe
Franklin Segura <fsegura56734@hotmail.com>
2017-04-01 09:56:16 -0400

    files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+module.exports = {
+  consumer_key: 'YpAM5fipkuLudv7Lse2uRnRl0',
+  consumer_secret: 'V0SRquJ0VvlD7XYHEHa3rVTJePR0DFYNvPtmbJOZS1oHiCAOZH',
+  access_token_key: 'V0SRquJ0VvlD7XYHEHa3rVTJePR0DFYNvPtmbJOZS1oHiCAOZH',
+  access_token_secret: 'nWPO08IWR1UYVu3hP0zVTJIXzGMubnF0Myu4GavXEimDs',
+}
---
+++ liri.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var fs = require('fs');
+var keys = require('./keys.js');
+
+var command = process.argv[2];
+var argument = process.argv[3];
+var output;
+
+
+if (command == "movie-this"){
+request('http://www.omdbapi.com/?t=' + argument, function (error, response, body) {
+    if (error){
+     console.log('error:', error); // Print the error if one occurred
+ } else {
+     console.log('body:', body); // Print the HTML for the Google homepage.
+ }
+
+
+});
+}
+
+if (command == "my-tweets"){    
+//creating a new object that allows my-tweets
+var client = new Twitter({
+ consumer_key: keys.consumer_key,
+ consumer_secret: keys.consumer_secret,
+ access_token_key: keys.access_token_key,
+ access_token_secret: keys.access_token_secret
+});
+
+var params = {screen_name: 'Frankiej871'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (!error) {
+   console.log(tweets);
+ }
+ if (error){
+     console.log(error);
+ }
+
+});
+}
